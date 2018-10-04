67124372d0b0d60354f7f5155869ce9384d638e1
John O'Keefe <jokeefe@weegolobal.com>
2018-10-04 15:01:42 -0400

    Lesson 09 complete

--- Lesson09-JS-VI/homework/homework.js
+++ Lesson09-JS-VI/homework/homework.js
@@ -1,32 +1,37 @@
 // Do not change any of the function names
 
 function invokeCallback(cb) {
-  // invoke cb
+    // invoke cb
+    return cb();
 }
 
 function sumArray(numbers, cb) {
-  // sum up all of the integers in the numbers array
-  // pass the result to cb
-  // no return is necessary
+    // sum up all of the integers in the numbers array
+    // pass the result to cb
+    // no return is necessary
+    cb(numbers.reduce((prev, curr) => prev + curr));
 }
 
 function forEach(arr, cb) {
-  // iterate over arr and pass its values to cb one by one
-  // hint: you will be invoking cb multiple times (once for each value in the array)
+    // iterate over arr and pass its values to cb one by one
+    // hint: you will be invoking cb multiple times (once for each value in the array)\
+    arr.forEach(item => cb(item));
 }
 
 function map(arr, cb) {
-  // create a new array
-  // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
-  // the new array should be the same length as the array argument
+    // create a new array
+    // iterate over each value in arr, pass it to cb, then place the value returned from cb into the new arr
+    // the new array should be the same length as the array argument
+    let newArray = arr.map(item => cb(item));
+    return [ ...newArray];
 }
 
 // Do not modify code below this line.
 // --------------------------------
 
 module.exports = {
-  invokeCallback,
-  sumArray,
-  forEach,
-  map,
+    invokeCallback,
+    sumArray,
+    forEach,
+    map
 };
