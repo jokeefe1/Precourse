68968d9a6c51ea5d1531618ae0ba2542716853d2
John O'Keefe <jokeefe@weegolobal.com>
2018-10-04 15:42:10 -0400

    Lesson 11 Complete

--- Lesson11-JS-VIII/homework/homework.js
+++ Lesson11-JS-VIII/homework/homework.js
@@ -3,20 +3,29 @@
 // solve these recursively
 
 function nFactorial(n) {
-  // return the factorial for n
-  // example:
-  // the factorial of 3 is 6 (3 * 2 * 1)
+    // return the factorial for n
+    // example:
+    // the factorial of 3 is 6 (3 * 2 * 1)
+    return n < 2 ? 1 : n * nFactorial(n - 1);
 }
 
 function nFibonacci(n) {
-  // fibonacci sequence: 1 2 3 5 8 13 ...
-  // return the nth number in the sequence
+    // fibonacci sequence: 1 2 3 5 8 13 ...
+    // return the nth number in the sequence
+    if (n < 2) {
+        return 1;
+    }
+
+    let num1 = n - 1;
+    let num2 = n - 2;
+
+    return nFactorial(num1) + nFactorial(num2);
 }
 
 // Do not modify code below this line.
 // --------------------------------
 
 module.exports = {
-  nFactorial,
-  nFibonacci,
+    nFactorial,
+    nFibonacci
 };
